bf78c48b3f6317e03628cf6dfab69940de7ef289
carlihos1121 <carandresdelrio1121@gmail.com>
2024-09-18 23:19:23 -0500

    Mapeado de todas las paginas del proyecto serviapp - Spring vrs 1.2

---
+++ src/main/java/com/example/ServiApp/controller/MapPagesController.java
@@ -0,0 +1,51 @@
+package com.example.ServiApp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+
+@Controller
+public class MapPagesController {
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/login")
+    public String login(){
+        return "iniciosesion";
+    }
+
+    @GetMapping("/registro")
+    public String registro(){
+        return "registro_usuario";
+    }
+
+    //Paginas funciones
+
+    @GetMapping("/gestionar-servicio")
+    public String gestionarServicio(){
+        return "gestionar_serv";
+    }
+
+    @GetMapping("/registrar-servicio")
+    public String registrarServicio(){
+        return "reg_servicio";
+    }
+
+    @GetMapping("/consejos-ahorro")
+    public String consejosAhorro(){
+        return "consejos_ahorro";
+    }
+
+    @GetMapping("/lineas-atencion")
+    public String lineasAtencion(){
+        return "Lineas_atencion";
+    }
+
+    @GetMapping("/perfil")
+    public String perfil(){
+        return "perfil_datos";
+    }
+
+}
