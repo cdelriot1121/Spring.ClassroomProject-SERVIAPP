d97e8598de40a20073f2f384efc97977feadfa0c
carlihos1121 <carandresdelrio1121@gmail.com>
2024-09-18 20:33:45 -0500

    first commit

---
+++ src/main/java/com/example/ServiApp/ServiAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.ServiApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ServiApp/ServiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ServiApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
