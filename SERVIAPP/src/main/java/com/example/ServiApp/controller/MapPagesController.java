e148e0e5e3df40a9e0f726188fd7afee6808ec91
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-10-28 23:00:40 -0500

    Cambios de valeria para el front

--- SERVIAPP/src/main/java/com/example/ServiApp/controller/MapPagesController.java
+++ SERVIAPP/src/main/java/com/example/ServiApp/controller/MapPagesController.java
@@ -7,95 +7,97 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class MapPagesController {
 
-    //Paginas principales abiertas a todos
-    @GetMapping("/")
-    public String index(){
-        return "index";
-    }
+   //Paginas principales abiertas a todos
+   @GetMapping("/")
+   public String index(){
+       return "index";
+   }
 
-    @GetMapping("/login")
-    public String login(){
-        return "iniciosesion";
-    }
+   @GetMapping("/login")
+   public String login(){
+       return "iniciosesion";
+   }
 
-    @GetMapping("/registro")
-    public String registro(){
-        return "registro_usuario";
-    }
+   @GetMapping("/registro")
+   public String registro(){
+       return "registro_usuario";
+   }
 
-    //Paginas funciones usuarios
+   //Paginas funciones usuarios
 
-    @GetMapping("/inicio")
-    public String inicio(){
-        return "interfaz_inicio";
-    }
+   @GetMapping("/inicio")
+   public String inicio(){
+       return "interfaz_inicio";
+   }
 
-    @GetMapping("/registrar-servicio")
-    public String registrarServicio(){
-        return "reg_servicio";
-    }
+   @GetMapping("/registrar-servicio")
+   public String registrarServicio(){
+       return "reg_servicio";
+   }
 
-    @GetMapping("/gestionar-servicio")
-    public String gestionarServicio(){
-        return "gestionar_serv";
-    }
+   @GetMapping("/gestionar-servicio")
+   public String gestionarServicio(){
+       return "gestionar_serv";
+   }
 
-    @GetMapping("/consejos-ahorro")
-    public String consejosAhorro(){
-        return "consejos_ahorro";
-    }
+   @GetMapping("/consejos-ahorro")
+   public String consejosAhorro(){
+       return "consejos_ahorro";
+   }
 
-    @GetMapping("/lineas-atencion")
-    public String lineasAtencion(){
-        return "Lineas_atencion";
-    }
+   @GetMapping("/lineas-atencion")
+   public String lineasAtencion(){
+       return "Lineas_atencion";
+   }
 
-    @GetMapping("/perfil")
-    public String perfil(){
-        return "perfil_datos";
-    }
+   @GetMapping("/perfil")
+   public String perfil(){
+       return "perfil_datos";
+   }
 
-    @GetMapping("/consejos-personzalidos")
-    public String consejospersonalizdos(){
-        return "consejos_personalizados";
-    }
+   @GetMapping("/consejos-personzalidos")
+   public String consejospersonalizdos(){
+       return "consejos_personalizados";
+   }
 
-    //Paginas admins
+   @GetMapping("/cortes_fallas")
+   public String cortesfallas() {
+       return "/cortes_fallas";
+   }
+   
+   @GetMapping ("/acercade")
+   public String acercade(){
+       return "/acerca_del_proyecto";
+   }
 
-    @GetMapping("/interfaz-admin")
-    public String interfazAdmin(){
-        return "interfaz-admin";
-    }
+   //Paginas admins
 
-    @GetMapping("/gestionar-servicios-admin")
-    public String gesServiciosAdmin(){
-        return "ges-servicios-admin";
-    }
+   @GetMapping("/interfaz-admin")
+   public String interfazAdmin(){
+       return "interfaz-admin";
+   }
 
-    @GetMapping("/login-admin")
-    public String loginAdmin(){
-        return "login-admin";
-    }
+   @GetMapping("/gestionar-servicios-admin")
+   public String gesServiciosAdmin(){
+       return "ges-servicios-admin";
+   }
+
+   @GetMapping("/login-admin")
+   public String loginAdmin(){
+       return "login-admin";
+   }
+
+   
+   @GetMapping("/reportes-admin")
+   public String reportesAdmin(){
+       return "reportes_admin";
+   }
 
     
-    @GetMapping("/reportes-admin")
-    public String reportesAdmin(){
-        return "reportes_admin";
-    }
-
-     
-    @GetMapping("/consejos-admin")
-    public String consejosAdmin(){
-        return "add_consejos_admin";
-    }
-
-    
-
-
-
-
-
-
+   @GetMapping("/consejos-admin")
+   public String consejosAdmin(){
+       return "add_consejos_admin";
+   }
 
 
 }
