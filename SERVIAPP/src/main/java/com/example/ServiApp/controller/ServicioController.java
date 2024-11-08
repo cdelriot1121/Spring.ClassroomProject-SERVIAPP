fac0d50021e8abc5dfe589392a101edb92d9d8f9
Gaos-Orozco <gorozcos@tecnocomfenalco.edu.co>
2024-11-08 13:44:39 -0500

    se puede eliminar servicios desde usuario

--- SERVIAPP/src/main/java/com/example/ServiApp/controller/ServicioController.java
+++ SERVIAPP/src/main/java/com/example/ServiApp/controller/ServicioController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -41,7 +42,19 @@ public class ServicioController {
         }
     }
 
+
     
-   
+
+
     
+
+    @PostMapping("/eliminar/{id}")
+    public String eliminarServicio(@PathVariable Long id){
+        servicioService.eliminarServicio(id);
+        return "redirect:/mis-servicios";
+    }
+
+
+
+
 }
