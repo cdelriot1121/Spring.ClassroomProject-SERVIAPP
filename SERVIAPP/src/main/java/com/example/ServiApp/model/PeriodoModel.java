5506eab1c2b047ee3bae8a0296e3628e26c39c75
cdelriot1121 <carandresdelrio1121@gmail.com>
2024-11-09 14:37:20 -0500

    PeriodoService ha sido creado y acondicionado principalmente para crear los periodos, ademas se ha actualizado el periodoReporitory para poder buscar periodo por id de servicio donde el servicio depende del id de usuario, creando este efecto en cascada y relacion.... Serviapp

--- SERVIAPP/src/main/java/com/example/ServiApp/model/PeriodoModel.java
+++ SERVIAPP/src/main/java/com/example/ServiApp/model/PeriodoModel.java
@@ -10,7 +10,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="periodo")
+@Table(name="periodos")
 
 public class PeriodoModel {
 
---
+++ SERVIAPP/src/main/java/com/example/ServiApp/services/PeriodoService.java
@@ -0,0 +1,28 @@
+package com.example.ServiApp.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ServiApp.model.PeriodoModel;
+import com.example.ServiApp.model.ServicioModel;
+import com.example.ServiApp.repository.PeriodoRepository;
+
+@Service
+public class PeriodoService {
+    @Autowired
+    private PeriodoRepository periodoRepository;
+
+    public PeriodoModel registrarPeriodo(PeriodoModel periodo){
+        return periodoRepository.save(periodo);
+    }
+
+    public List<PeriodoModel> obtenerPeriodosPorServicios(ServicioModel servicio){
+        return periodoRepository.findByServicio(servicio);
+    }
+
+    
+
+
+}
