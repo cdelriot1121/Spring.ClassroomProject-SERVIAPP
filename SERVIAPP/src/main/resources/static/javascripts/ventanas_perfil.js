f349a5ea5467fd84466cb04ef50405f694d3778c
Sir Willys <whurtadob@tecnocomfenalco.edu.co>
2024-11-20 17:45:31 -0500

    Mensaje de sweet alert en contraseña (revision)

--- SERVIAPP/src/main/resources/static/javascripts/ventanas_perfil.js
+++ SERVIAPP/src/main/resources/static/javascripts/ventanas_perfil.js
@@ -160,7 +160,7 @@ function confirmarCambioServicio(event) {
 
 
 function confirmarEliminarServicio(event, servicioId) {
-    event.preventDefault();  // Evitar que el formulario se envíe inmediatamente
+    event.preventDefault();  
 
     Swal.fire({
         title: '¿Estás seguro de eliminar este servicio?',
@@ -198,9 +198,24 @@ function confirmarEliminarServicio(event, servicioId) {
 
 
 //alerta para la pagina de cambiar contraseña
-
 function confirmarCambioContraseña(event) {
     event.preventDefault(); 
+
+    const currentPassword = document.getElementById('current-password').value;
+    const newPassword = document.getElementById('new-password').value;
+    const confirmPassword = document.getElementById('confirm-password').value;
+
+   
+    if (newPassword !== confirmPassword) {
+        Swal.fire('Error', 'Las contraseñas no coinciden.', 'error');
+        return;
+    }
+
+    const passwordPattern = /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+    if (!passwordPattern.test(newPassword)) {
+        Swal.fire('Error', 'La nueva contraseña no cumple con los requisitos.', 'error');
+        return;
+    }
 
     Swal.fire({
         title: '¿Estás seguro de cambiar tu contraseña?',
@@ -209,7 +224,7 @@ function confirmarCambioContraseña(event) {
         showCancelButton: true,
         confirmButtonText: 'Sí, cambiar contraseña',
         cancelButtonText: 'No, cancelar',
-        background: 'linear-gradient(135deg, #f0f8ff, #d0e7ff)', 
+        background: 'linear-gradient(135deg, #f0f8ff, #d0e7ff)',
         customClass: {
             popup: 'swal-custom-popup',
             confirmButton: 'btn-confirmar',
@@ -221,45 +236,14 @@ function confirmarCambioContraseña(event) {
             logo.style.width = '155px';
             logo.style.display = 'block';
             logo.style.margin = '0 auto 10px';
-
             Swal.getIcon().before(logo);
         }
     }).then((result) => {
         if (result.isConfirmed) {
-          
-            document.getElementById('changePasswordForm').submit(); 
+         
+            document.getElementById('changePasswordForm').submit();
         } else {
             Swal.fire('No se ha realizado ningún cambio', 'Tu contraseña no ha sido modificada', 'info');
         }
     });
 }
-
-
-
-
-
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-//miau
-
