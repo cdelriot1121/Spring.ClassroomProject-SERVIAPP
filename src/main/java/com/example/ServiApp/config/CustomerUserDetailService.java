4f1d1f0149ae165a9b25c5771c979a8e82cdb256
cdelriot1121 <carandresdelrio1121@gmail.com>
2025-04-11 13:03:31 -0500

    add: Añadir archivos necesarios para comenzar la configuracion de SpringSecurity, que se va a programar durante esta semana aplicado en Serviapp

---
+++ src/main/java/com/example/ServiApp/config/CustomerUserDetailService.java
@@ -0,0 +1,8 @@
+package com.example.ServiApp.config;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerUserDetailService {
+
+}
---
+++ src/main/java/com/example/ServiApp/config/SecurityConfiguration.java
@@ -0,0 +1,9 @@
+package com.example.ServiApp.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SecurityConfiguration {
+    
+
+}
