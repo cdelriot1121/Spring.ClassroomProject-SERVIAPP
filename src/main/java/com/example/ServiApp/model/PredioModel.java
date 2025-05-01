fa42acb75186186992eadaf6c778315527ccad53
Sirwillys06 <whurtado180@gmail.com>
2025-05-01 18:50:06 -0500

    integracion de redis para la sesion

--- src/main/java/com/example/ServiApp/model/PredioModel.java
+++ src/main/java/com/example/ServiApp/model/PredioModel.java
@@ -1,8 +1,22 @@
 package com.example.ServiApp.model;
 
+import java.io.Serializable;
+
 import org.springframework.data.mongodb.core.mapping.Field;
 
-public class PredioModel {
+
+// La serialización es el proceso de convertir un objeto en una secuencia de bytes,
+// lo que permite que dicho objeto sea almacenado (por ejemplo, en Redis) o transmitido (por ejemplo, a través de la red).
+// En Java, un objeto debe implementar la interfaz java.io.Serializable para que pueda ser serializado.
+// Cuando usamos Redis como almacén de sesiones (como con Spring Session),
+// los objetos que se guardan en la sesión —como usuarios autenticados— deben ser serializables.
+// Si intentamos guardar un objeto no serializable, se lanzará una excepción como NotSerializableException.
+// Al registrar un serializador (como RedisSerializer.java()), le decimos a Spring cómo convertir los objetos en bytes
+// para almacenarlos en Redis y luego reconstruirlos cuando sea necesario.
+// En este caso, estamos utilizando RedisSerializer.java() para serializar objetos Java de forma predeterminada.
+
+public class PredioModel implements Serializable {
+    private static final long serialVersionUID = 1L;
 
     @Field(name = "barrio")
     private String barrio;
